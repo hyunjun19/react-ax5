82c4bce252be4bd5bbf228d31e7bdf4bc2b02c47
HJ.Park <hyunjun19@gmail.com>
2017-07-22 01:28:40 +0900

    Enzyme 테스트 방식 테스트

--- src/components/AXLayout.test.tsx
+++ src/components/AXLayout.test.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import * as enzyme from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import AXLayout from './AXLayout';
 
 describe('<AXLayout />', () => {
-  it('AXLayout DOM rendering test', () => {
-    const axLayout = enzyme.shallow(<AXLayout />);
-    expect(axLayout.prop('data-ax5layout')).toEqual('ax5');
+  it('[shallow] should render [data-axrlayout="ax5"] prop.', () => {
+    const wrapper = shallow(<AXLayout />);
+    console.log('shallow', wrapper.debug());
+    expect(wrapper.prop('data-ax5layout')).toEqual('ax5');
+  });
+
+  it('[mount] should render [data-axrlayout="ax5"] prop.', () => {
+    const wrapper = mount(<AXLayout />);
+    console.log('mount', wrapper.debug());
+    expect(wrapper.find('[data-ax5layout="ax5"]').length).toEqual(1);
+  });
+
+  it('[render] should render [data-axrlayout="ax5"] prop.', () => {
+    const wrapper = render(<AXLayout />);
+    console.log('render', wrapper.html());
+    expect(wrapper.find('[data-ax5layout="ax5"]').length).toEqual(1);
   });
 });
